3d89c69a164a2cc0e5bc185f117b85219122a77b
daniel Velazquez <daniel_velazquez96@outlook.es>
2022-01-05 16:15:44 -0600

    Se adelanto parte de la logica del juego

--- src/js/memorama-page.js
+++ src/js/memorama-page.js
@@ -1,22 +1,27 @@
 
 // Variable de libreria underscores
-var _ = require('underscore');
+let _ = require('underscore');
 
 // get elements by DOM
 const NameJugador1=document.getElementById('nombreJugador1');
 const NameJugador2=document.getElementById('nombreJugador2');
 
-const CountJugador1=document.getElementById('conteoJugador1');
-const CountJugador2=document.getElementById('conteoJugador2');
+const PointsJugador1=document.getElementById('conteoJugador1');
+const PointsJugador2=document.getElementById('conteoJugador2');
 
 const cartas=document.querySelectorAll('.container .card')
 const cartasImg=document.querySelectorAll('.container .card img')
 
 // Game Variables
-let conteoJugaodr1;
-let conteoJugaodr2;
+let points=[0,0];
 
-let deck=[];
+let deck;
+
+let turnoPlayer,
+    turno,
+    parSelectCards;
+
+
 
 
 const htmlNames=()=>{
@@ -29,13 +34,14 @@ const htmlNames=()=>{
 }
 
 const events=()=>{
-
     cartas.forEach((carta,index)=>{
-        carta.addEventListener('click',()=>{
 
+        carta.addEventListener('click',()=>{
             carta.classList.toggle('facedown');
-           
+            game(index);
+
         })
+
     })
 
 }
@@ -55,22 +61,104 @@ const placeCards=()=>{
     })
 }
 
+const changeColorTurno=(turno)=>{
+    const colorActivePlayer='blue';
+    if(!turno){
+        NameJugador1.style.color=colorActivePlayer;
+        PointsJugador1.style.color=colorActivePlayer;
+        NameJugador2.style.color='black';
+        PointsJugador2.style.color='black';
+    }else{
+        NameJugador1.style.color='black';
+        PointsJugador1.style.color='black';
+        NameJugador2.style.color=colorActivePlayer;
+        PointsJugador2.style.color=colorActivePlayer;
+    }
+}
+
+
+const game=(SelectCard)=>{
+    turno++;
+    if(!(turno%2==0)){
+        parSelectCards.push(SelectCard);
+    }else{
+        parSelectCards.push(SelectCard);
+        // Validar si acerto o no acerto
+        if(winOrLoseTurn(parSelectCards)){
+            points[turnoPlayer]++;
+            addpoint();
+            // addpoint();
+
+            // cartas[parSelectCards[0]].style='cursor: not-allowed;';
+            // cartas[parSelectCards[1]].style='cursor: not-allowed;';
+            cartas[parSelectCards[0]].removeEventListener('click',this,false);
+            cartas[parSelectCards[1]].removeEventListener('click',this,false);
+           
+            parSelectCards=[];
+            
+        }else{
+
+            setTimeout(()=>{
+                cartas[parSelectCards[0]].classList.toggle('facedown');
+                cartas[parSelectCards[1]].classList.toggle('facedown');
+                parSelectCards=[];
+            }, 1500);
+            
+                
+        }
+        turnoPlayer=(turnoPlayer) ? 0 : 1 ;
+        changeColorTurno(turnoPlayer);
+    }
+}
+
+const winOrLoseTurn=([cart1,cart2])=>{
+
+    return (deck[cart1]==1 && deck[cart2]==9)?1:
+            (deck[cart1]==2 && deck[cart2]==10)?1:
+            (deck[cart1]==3 && deck[cart2]==11)?1:
+            (deck[cart1]==4 && deck[cart2]==12)?1:
+            (deck[cart1]==5 && deck[cart2]==13)?1:
+            (deck[cart1]==6 && deck[cart2]==14)?1:
+            (deck[cart1]==7 && deck[cart2]==15)?1:
+            (deck[cart1]==8 && deck[cart2]==16)?1:
+            (deck[cart1]==9 && deck[cart2]==1)?1:
+            (deck[cart1]==10 && deck[cart2]==2)?1:
+            (deck[cart1]==11 && deck[cart2]==3)?1:
+            (deck[cart1]==12 && deck[cart2]==4)?1:
+            (deck[cart1]==13 && deck[cart2]==5)?1:
+            (deck[cart1]==14 && deck[cart2]==6)?1:
+            (deck[cart1]==15 && deck[cart2]==7)?1:
+            (deck[cart1]==16 && deck[cart2]==8)?1:
+            0;
+}
+
+
+const addpoint=()=>{
+    PointsJugador1.innerHTML=points[0];
+    PointsJugador2.innerHTML=points[1];
+}
 
 const init=()=>{
     
     // htmlNames();
 
-    conteoJugaodr1=0;
-    conteoJugaodr2=0;
-
-    CountJugador1.innerHTML=conteoJugaodr1;
-    CountJugador2.innerHTML=conteoJugaodr2;
-    
+    deck=[];
     initDeck();
 
-    events()
+    turnoPlayer=0;
+    changeColorTurno(turnoPlayer);
+    
+    points=[0,0];
 
+    addpoint();
+    
+    turno=0;
+    parSelectCards=[];
+
+    events()
     placeCards();
+
+    
 }
 
 
